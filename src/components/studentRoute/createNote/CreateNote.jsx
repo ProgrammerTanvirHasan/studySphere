d7f68833d66bee5b2c3b0a7a4d71315d9ca85bd0
Your Name <you@example.com>
2024-12-23 17:35:00 +0600

    added store note

--- src/components/studentRoute/createNote/CreateNote.jsx
+++ src/components/studentRoute/createNote/CreateNote.jsx
@@ -1,7 +1,77 @@
+import { useContext } from "react";
+import { AuthContext } from "../../../AuthProvider";
+import Swal from "sweetalert2";
+
 const CreateNote = () => {
+  const { user } = useContext(AuthContext);
+  const handleNote = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const title = form.title.value;
+    const email = form.email.value;
+    const note = form.note.value;
+    const notes = { title, email, note };
+    console.log(notes);
+
+    fetch("http://localhost:4000/stored", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(notes),
+    })
+      .then((res) => res.json())
+      .then(() => {
+        Swal.fire({
+          title: "Good job!",
+          text: "Added Your Note",
+          icon: "success",
+        });
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+    form.reset();
+  };
+
   return (
-    <div>
-      <h2>createNote</h2>
+    <div className="bg-green-300 px-2 lg:w-2/3 h-2/3">
+      <h2 className="bg-orange-400 py-2 text-white text-center text-xl">
+        Create Your Personal Note
+      </h2>
+      <form onSubmit={handleNote}>
+        <div className="mt-4">
+          <label className="input input-bordered flex items-center gap-2 lg:w-3/5 mx-auto">
+            Title
+            <input type="text" name="title" className="grow" />
+          </label>
+
+          <label className="input input-bordered flex items-center gap-2 lg:w-3/5 mx-auto my-2">
+            Email
+            <input
+              type="email"
+              name="email"
+              defaultValue={user?.email}
+              className="grow"
+              required
+            />
+          </label>
+
+          <div className="lg:w-3/5 mx-auto">
+            <textarea
+              placeholder="Note"
+              name="note"
+              className="textarea textarea-bordered textarea-lg w-full"
+            ></textarea>
+          </div>
+
+          <div className="text-center">
+            <button className="btn bg-black text-white mt-4 w-full lg:w-3/5 text-lg">
+              Store
+            </button>
+          </div>
+        </div>
+      </form>
     </div>
   );
 };
--- src/components/studentRoute/personalNote/PersonalNote.jsx
+++ src/components/studentRoute/personalNote/PersonalNote.jsx
@@ -1,7 +1,58 @@
+import { useQuery } from "@tanstack/react-query";
+import { useContext } from "react";
+import { AuthContext } from "../../../AuthProvider";
+import Marquee from "react-fast-marquee";
 const PersonalNote = () => {
+  const { user } = useContext(AuthContext);
+  const email = user?.email;
+  const { isPending, error, data } = useQuery({
+    queryKey: ["storeData", email],
+    queryFn: () =>
+      fetch(`http://localhost:4000/stored/${email}`).then((res) => res.json()),
+  });
+
+  if (isPending) return "Loading...";
+
+  if (error) return "An error has occurred: " + error.message;
+
   return (
-    <div>
-      <h2>personal note</h2>
+    <div className="bg-indigo-950 max-w-full min-h-screen">
+      <h2 className="bg-orange-400 py-2 text-center text-white text-xl">
+        Personal Note That You Stored
+      </h2>
+      {data.length === 0 ? (
+        <>
+          <div className="w-[780px] h-[780px]  mt-4 overflow-hidden flex items-center justify-center">
+            <Marquee
+              direction="down"
+              speed={100}
+              gradient={false}
+              className="w-full h-full"
+              pauseOnHover
+            >
+              <Marquee
+                direction="alternate"
+                speed={50}
+                gradient={false}
+                pauseOnHover
+              >
+                <p className="text-center  font-bold text-6xl text-red-600">
+                  No Note Yet
+                </p>
+              </Marquee>
+            </Marquee>
+          </div>
+        </>
+      ) : (
+        <div className="px-4 ">
+          {data.map((note) => (
+            <div key={note._id}>
+              <h2 className="text-2xl text-white">Title :{note.title}</h2>{" "}
+              <p className="text-white text-sm mb-8">{note.note}</p>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
