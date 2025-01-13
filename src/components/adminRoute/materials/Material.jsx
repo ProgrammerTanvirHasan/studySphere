e0f81cd7a3b86ed3c852fed26554d6802eec8ad4
Your Name <you@example.com>
2025-01-13 15:40:10 +0600

    added admin material route

---
+++ src/components/adminRoute/materials/Material.jsx
@@ -0,0 +1,70 @@
+import { FaGoogleDrive } from "react-icons/fa";
+import { Link } from "react-router";
+import Swal from "sweetalert2";
+
+const Material = ({ items }) => {
+  const { title, studySessionId, tutorEmail, driveLink, imageUrl, _id } = items;
+
+  const handleDelete = (_id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        fetch(`http://localhost:4000/material/${_id}`, {
+          method: "DELETE",
+        }).then((response) => {
+          if (response.ok) {
+            Swal.fire({
+              title: "Deleted!",
+              text: "Your file has been deleted.",
+              icon: "success",
+            });
+          } else {
+            Swal.fire({
+              title: "Error!",
+              text: "Failed to delete the file.",
+              icon: "error",
+            });
+          }
+        });
+      }
+    });
+  };
+
+  return (
+    <div className="pt-8 w-96  shadow-2xl rounded-md">
+      <img className="w-96 h-64" src={imageUrl} alt={title} />
+      <div className="flex justify-between">
+        <p className="text-orange-400">{studySessionId}</p>
+        <div className="flex ">
+          <p className="text-2xl">
+            <FaGoogleDrive></FaGoogleDrive>
+          </p>
+          <Link to={driveLink}>
+            <button className="text-sm  border-b-4 border-black">
+              Google Drive
+            </button>
+          </Link>
+        </div>
+      </div>
+      <h2 className="card-title ">{title}</h2>
+      <p>{tutorEmail}</p>
+      <div className="pt-6 pb-2">
+        <button
+          onClick={() => handleDelete(_id)}
+          className="bg-red-700 text-white px-2 rounded-sm"
+        >
+          Remove Content
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Material;
--- src/components/adminRoute/materials/Materials.jsx
+++ src/components/adminRoute/materials/Materials.jsx
@@ -1,7 +1,26 @@
+import { useQuery } from "@tanstack/react-query";
+import Material from "./Material";
+
 const Materials = () => {
+  const { isPending, error, data } = useQuery({
+    queryKey: ["material"],
+    queryFn: () =>
+      fetch("http://localhost:4000/material").then((res) => res.json()),
+  });
+
+  if (isPending) return "Loading...";
+
+  if (error) return "An error has occurred: " + error.message;
   return (
     <div>
-      <h2>materials</h2>
+      <h2 className="py-2 text-center text-xl bg-neutral-800 text-white">
+        All materials created by tutor
+      </h2>
+      <div className="grid lg:grid-cols-2 ml-8">
+        {data.map((items) => (
+          <Material key={items._id} items={items}></Material>
+        ))}
+      </div>
     </div>
   );
 };
